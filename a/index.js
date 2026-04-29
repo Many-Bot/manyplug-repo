a46496ad1c2bf034c168fa67f5f117d3aaf333f3
synt-xerror <169557594+synt-xerror@users.noreply.github.com>
2026-04-29 17:48:40 -0300

    removed plugin 'a'

--- a/index.js
+++
@@ -1,9 +0,0 @@
-import { hangmanActive } from "../forca/index.js";
-
-export default async function ({ msg }) {
-  if (msg.body.trim().toLowerCase() !== "a") return;
-  if (msg.args.length > 1) return;
-  if (hangmanActive) return;
-
-  await msg.reply("B!");
-} 
